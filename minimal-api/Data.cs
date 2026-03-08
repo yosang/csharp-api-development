66b0f8ea784dfe384d2d4c708623d3ca7d9ff585
Yosmel Chiang <yosmelchiang@gmail.com>
2026-03-08 21:16:14 +0100

    feat: implement crud operations

--- minimal-api/Data.cs
+++ minimal-api/Data.cs
@@ -2,6 +2,7 @@ namespace minimal.Data;
 
 public class User
 {
+    public int Id { get; set; }
     public string Name { get; set; } = string.Empty;
     public int Age { get; set; }
 }
--- minimal-api/Program.cs
+++ minimal-api/Program.cs
@@ -5,24 +5,57 @@ var app = builder.Build();
 
 List<User> users = new()
 {
-    new User() { Name = "John", Age = 33 },
-    new User() { Name = "Maria", Age = 43 },
-    new User() { Name = "Carlos", Age = 50 },
-    new User() { Name = "Jose", Age = 32 },
+    new User() { Id = 1, Name = "John", Age = 33 },
+    new User() { Id = 2, Name = "Maria", Age = 43 },
+    new User() { Id = 3, Name = "Carlos", Age = 50 },
+    new User() { Id = 4, Name = "Jose", Age = 32 },
 };
 
 
 app.MapGet("/", () => "Hello World!");
-app.MapGet("/users", () => users); // Endpoint /users - 
-app.MapGet("item/{id}", () => "one item"); // Endpoint with route paramater
+
+app.MapGet("/users", () => users); // Returns all users, notice how the API automatically serializes the response to JSON
+
+app.MapGet("users/{id}", (int id) => // Endpoint with route paramater
+{
+    var user = users.SingleOrDefault(e => e.Id == id);
+    if (user == null) return Results.NotFound();
+    return Results.Ok(user);
+});
+
+app.MapPost("/users", (User user) =>
+{
+    user.Id = users.Max(e => e.Id) + 1; // autoincrement
+    users.Add(user);
+    return Results.Created();
+
+});
+
+app.MapPut("/users/{id}", (int id, User payload) =>
+{
+    var user = users.SingleOrDefault(e => e.Id == id);
+    if (user == null) return Results.NotFound();
+
+    user.Name = payload.Name;
+    user.Age = payload.Age;
+
+    return Results.Ok();
+});
+
+app.MapDelete("/users/{id}", (int id) =>
+{
+    var user = users.SingleOrDefault(e => e.Id == id);
+    if (user == null) return Results.NotFound();
+
+    users.Remove(user);
+
+    return Results.Ok();
+});
 
 /*
-
-MapGet()
-MapPost()
-MapPut()
-MapDelete()
-
+- Automatic JSON Serialization
+- Lightweight and concise, no need for controllers
+- HTTP Response methods: Results.Ok(), Results.NotFound(), Results.Created() for proper Status Codes
 */
 
 app.Run();
